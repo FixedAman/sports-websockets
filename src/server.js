83fba726de25e6ca7ca5aa2fed635f76edea1ff0
Aman Mahish <amanmahish73@gmail.com>
2026-03-07 17:21:01 +0530

    adding utils and successfully inserting data in the database

--- src/server.js
+++ src/server.js
@@ -1,13 +1,12 @@
 import express from "express";
-import { matchRouter } from "./routes/matches";
+import { matchRouter } from "./routes/matches.js";
+import "dotenv/config";
 const app = express();
 const port = 8000;
-
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("Hello from server");
-});
+
 app.use("/", matchRouter);
+app.use("/matches", matchRouter);
 app.listen(port, () => {
   console.log(`server is started at ${port}`);
 });
