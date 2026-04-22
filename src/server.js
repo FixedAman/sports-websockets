d8ad0724d322389154474e681ae544d928281abd
Aman Mahish <amanmahish73@gmail.com>
2026-04-22 23:17:20 +0530

    fix some changes as ai says it will be good to use arcjet before connection to websocket

--- src/server.js
+++ src/server.js
@@ -10,15 +10,15 @@ const HOST = process.env.HOST || "0.0.0.0";
 
 app.use(express.json());
 const server = http.createServer(app);
+app.use(securityMiddleware());
 app.use("/", matchRouter);
 app.use("/matches", matchRouter);
-app.use(securityMiddleware());
 const { broadcastMatchCreated } = attachWebSocketServer(server);
 
 app.locals.broadcastMatchCreated = broadcastMatchCreated;
 server.listen(PORT, HOST, () => {
   const baseUrl =
-    HOST === "0.0.0.0" ? `http://localhost:${PORT}` : `http://${HOST}:PORT`;
+    HOST === "0.0.0.0" ? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`;
   console.log(`Server is running on ${baseUrl}`);
   console.log(`Websocket is running  on ${baseUrl.replace("http", "ws")}/ws`);
 });
