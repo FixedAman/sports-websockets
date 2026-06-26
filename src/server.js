1acf80379b267b9ceea833ce580acd9297946dab
Aman Mahish <amanmahish73@gmail.com>
2026-06-26 18:32:16 +0530

    urlchanged

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://sports-websockets-ten.vercel.app/",
+      "https://sports-websockets-ten.vercel.app",
     ],
   }),
 );
